2cfeded94947f657e033c893eb2b31ed0b130e25
Anh Phan Duc <kanzungjak@gmail.com>
2015-05-12 15:54:19 +0200

    first commit

---
+++ CA.java
@@ -0,0 +1,195 @@
+import java.util.Arrays;
+
+class CA {
+	
+	int[] state1;
+	int[] state2;
+	int[] nextState;
+	int[] G_state;
+	int[] G_state2;
+
+	static int round;
+
+	CA(String startConfig) {
+		this.state1 = new int[startConfig.length()+4];
+		this.state2 = new int[startConfig.length()+4];
+		this.G_state = new int[startConfig.length()+4]; //Granularitätszustand
+		this.nextState = new int[startConfig.length()+4];
+
+		for (int i=0; i < state1.length; i++) {
+			this.state1[i] = 0;
+			this.G_state[i] = 0;
+		}
+		for (int i=2; i < state1.length-2; i++) {
+			this.state2[i+2] = Integer.parseInt( "" + startConfig.charAt(i) );
+		}
+
+		round = 0;
+	}
+
+
+
+	//wandelt eine Dezimalzahl in einen binären Regelsatz um, auf eine sehr murksige Art und Weise
+	static int[] createRuleset(long decimal, int radius, int numStates) {
+		//Die Werte sollten mit Null initiert sein
+		//Anzahl Regeln = |numStates|**(2r+1)
+		int numRules = (int)Math.pow(numStates, 2*radius+1) ;
+		int[] ruleset = new int[numRules];
+
+		String s = Long.toBinaryString(decimal);
+
+		for(int i=0; i < s.length(); i++) {
+			ruleset[ numRules - s.length() + i ] = Integer.parseInt( "" + s.charAt(i) );
+		}
+		//System.out.println( Arrays.toString(ruleset) );
+		return ruleset;
+	}
+
+	public static int getBit(int n, int k) {
+    	return (n >> k) & 1;
+	}
+
+	public static int[] arrayAdd(int[] a1, int[] a2) {
+		int[] newArray = new int[a1.length];
+		if (a1.length != a2.length) {
+			System.out.println("Arrays nicht gleich lang!");
+			return a1;
+		}
+
+		for(int i=0; i<a1.length; i++) {
+			newArray[i] = (a1[i] + a2[i]) % 2;
+		}
+		return newArray;
+	}
+
+	public static void printState(int[] state) {
+		for(int i=0; i < state.length; i++) {
+			if(state[i] == 1) {
+				System.out.print("#");
+			} else if(state == 0) {
+				System.out.print("_");
+			} else {
+				System.out.print(state[i]);
+			}
+		}
+		System.out.println("");
+	}
+
+	/*deprecated
+	//r=1, Zustände={0,1}
+	public static int[] step(int[] state1, int[] state2) {
+		int[] ruleset = new int[8];
+		int[] nextState = new int[state1.length];
+
+		for(int i=0; i < state1.length; i++) {
+			String nb;
+			int index;
+			//Wie sieht die lokale Nachbarschaft aus?
+			//Null Boundary Condition
+			if (i == 0) { //kein linker Nachbar
+				nb = "" + 0 + state2[i] + state2[i+1];
+			} else if (i == state2.length - 1) { //kein rechter Nachbar
+				nb = "" + state2[i-1] + state2[i] + 0; 
+			} else { //normaler Fall
+				nb = "" + state2[i-1] + state2[i] + state2[i+1];
+			}
+			index = Integer.parseInt(nb,2);
+
+			if (state1[i] == 1) {
+				ruleset = createRuleset(100, 1);
+				nextState[i] = ruleset[index];
+			} else { 
+				ruleset = createRuleset(155, 1);
+				nextState[i] = ruleset[index];
+			}
+		}
+		return nextState;
+	}
+	*/
+
+	//r=1, Zustände = {0,1,2,3}
+	/*
+	Input: Zustand bei t-1 für Zellen index + index+1
+		   Zustand bei t
+	Output: Zustand bei t+1 für Zelle index und index +1
+	*/
+	public static int[] localstep_bulk (int[] state1, int[] state2, int index, int[] ruleset) {
+		int[] nextState = new int[2];
+		//int[] ruleset = createRuleset(rule, 1, 4); // |Zustände|**|Anzahl Elemente| => 4**3 = 64 bits
+		//der Zustand einer bulky Zelle besteht aus 2-Bits
+		//deshalb müssen wir erst 2-bits als Dezimalzahl interpretieren (eigentlich 4-näre Zahl)
+		//und nach noch einen Wert weiter schauen, umso die richtige lokale Nachbarschaft zu erhalten
+		int ownState =  Integer.parseInt( "" + state2[index] + state2[ index+1 ], 2);
+		String nb = "" + state2[ index-1 ] + ownState + state2[ index+2 ];
+		int ruleIndex = Integer.parseInt(nb, 4);
+
+		int firstBit = getBit(ruleset[ruleIndex], 0); //LSB
+		int secondBit = getBit(ruleset[ruleIndex], 1);
+		//????stimmt die Reihenfolge denn??????
+		nextState[1] = (firstBit + state1[index]) % 2;
+		nextState[0] = (secondBit + state1[ index+1 ]) % 2;
+
+		return nextState; 
+	}
+
+	public static int localstep_fine (int[] state1, int[] state2, int index, int[] ruleset) {
+		//int ruleset = createRuleset(rule, 2, 2);
+		String nb = "" + state2[index-2] + state2[index-1] + state2[index] + state2[index+1] + state2[index+2];
+		int ruleIndex = Integer.parseInt(nb, 2);
+
+		return (ruleset[ruleIndex] + state1[index]) % 2;
+	}
+
+	public static int[] globalstep(int[] state1, int[] state2, int G_state, int fineRule, int bulkRule) {
+		int[] fineRuleset = createRuleset(fineRule, 2, 2); //radius=2, #states=2
+		int[] bulkRuleset = createRuleset(bulkRule, 1, 4);
+		int[] newState = new int[state1.length];
+
+		for(int i=2; i<state1.length-2; i++) {
+
+			if (G_state[i] == 1) { //bulky cell
+				int[] tmp = localstep_bulk(state1, state2, i , bulkRuleset);
+				newState[ i ] = tmp[0];
+				newState[i+1] = tmp[1];
+				i++;
+			} else { //fine cell
+				newState[i] = localstep_fine(state1, state2, i, fineRuleset);
+			}
+
+		}
+
+		return newState;
+
+	}
+
+	public static void main(String[] args) {
+		CA ca = new CA("000000000000000000000000000000010000000000000000000000000000000000");
+		boolean run = false;
+		long bulkrule = 2319301L;
+		long finerule = 21312L;
+
+		//step_bulk(ca.state1, ca.state2, rule, ca.G_state);
+
+		if (run) {
+			//	printState(ca.state1);
+			//	printState(ca.state2);
+			while (round < 20) {
+
+				if (round % 2 == 0) {
+					ca.nextState = step_r2(ca.state1, ca.state2, rule);
+					ca.state1 = ca.nextState;
+					ca.G_state = arrayAdd(ca.G_state, ca.state1);
+				} else {
+					ca.nextState = step_r2(ca.state2, ca.state1, rule);
+					round++;
+					ca.state2 = ca.nextState;
+					ca.G_state = arrayAdd(ca.G_state, ca.state2);
+				}
+				round++;
+			//	printState(ca.nextState);
+				printState(ca.G_state);
+			}
+		}
+	}
+
+}
